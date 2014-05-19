1ca740874853ed1ebbffe6bf330328ad87e85f1c
Guilherme Pereira <jorgeguilhermepereira@gmail.com>
2014-05-19 17:10:19 +0100

    Merge origin/master
    Conflicts:
    	web/index.xhtml

---
+++ src/java/pt/uc/aor/edcodisrt/entities/UserApp.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.uc.aor.edcodisrt.entities;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author Guilherme Pereira
+ */
+@Entity
+public class UserApp implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    
+    private String password;
+    
+    @OneToMany(mappedBy = "userApp", cascade = {CascadeType.MERGE, CascadeType.PERSIST}, orphanRemoval = true)
+    private List<Snapshot> snapshotList;
+
+    public List<Snapshot> getSnapshotList() {
+        return snapshotList;
+    }
+
+    public void setSnapshotList(List<Snapshot> snapshotList) {
+        this.snapshotList = snapshotList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof UserApp)) {
+            return false;
+        }
+        UserApp other = (UserApp) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "pt.uc.aor.edcodisrt.entities.User[ id=" + id + " ]";
+    }
+
+}
