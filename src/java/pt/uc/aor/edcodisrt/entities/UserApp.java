81e554fdaf1bac61123096fc29c614d0c50bece4
elsasantos <elsateodoros@gmail.com>
2014-05-20 12:07:57 +0100

    Merge origin/master
    Conflicts:
    	WebServiceAPI/src/java/pt/uc/aor/webservice/facade/ProductFacade.java

--- src/java/pt/uc/aor/edcodisrt/entities/UserApp.java
+++ src/java/pt/uc/aor/edcodisrt/entities/UserApp.java
@@ -12,6 +12,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 /**
@@ -33,7 +34,18 @@ public class UserApp implements Serializable {
     @OneToMany(mappedBy = "userApp", cascade = {CascadeType.MERGE, CascadeType.PERSIST}, orphanRemoval = true)
     private List<Snapshot> snapshotList;
 
+    @ManyToOne
+    private Group group;
+
     public UserApp() {
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
     }
 
     public List<Snapshot> getSnapshotList() {
