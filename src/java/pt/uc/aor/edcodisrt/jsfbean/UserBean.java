df831519f8184f3a16285e755195a0207fd0d5e8
elsasantos <elsateodoros@gmail.com>
2014-05-22 18:11:05 +0100

    Template e peers

--- src/java/pt/uc/aor/edcodisrt/jsfbean/UserBean.java
+++ src/java/pt/uc/aor/edcodisrt/jsfbean/UserBean.java
@@ -6,10 +6,18 @@
 package pt.uc.aor.edcodisrt.jsfbean;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.PrePersist;
+import pt.uc.aor.edcodisrt.endpoint.MyWhiteboard;
+import pt.uc.aor.edcodisrt.entities.Snapshot;
 import pt.uc.aor.edcodisrt.entities.UserApp;
+import pt.uc.aor.edcodisrt.facades.SnapshotFacade;
 import pt.uc.aor.edcodisrt.facades.UserAppFacade;
 
 /**
@@ -21,19 +29,58 @@ import pt.uc.aor.edcodisrt.facades.UserAppFacade;
 public class UserBean implements Serializable {
 
     @Inject
-    private UserAppFacade userfacade;
+    private UserAppFacade userFacade;
+    @Inject
+    private SnapshotFacade snapFacade;
+    @Inject
+    private MyWhiteboard myWhiteboard;
     private String username;
     private UserApp user;
+    private Date actualdate;
+
+    @PrePersist
+    public void onCreate() {
+        GregorianCalendar gc = new GregorianCalendar();
+        this.setActualdate(gc.getTime());
+    }
 
     public UserBean() {
+        this.user = new UserApp();
     }
 
-    public UserAppFacade getUserfacade() {
-        return userfacade;
+    public String loggedUser() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        this.username = context.getExternalContext().getRemoteUser();
+        return username;
     }
 
-    public void setUserfacade(UserAppFacade userfacade) {
-        this.userfacade = userfacade;
+    public void saveWhiteboard() {
+        Snapshot snap = new Snapshot();
+
+        //procura na entidade UserApp qual o user logado;:
+        List<UserApp> listUser = userFacade.findAll();
+        for (UserApp u : listUser) {
+            if (u.getName().equals(username)) {
+                this.user = u;
+            }
+        }
+
+        //vai buscar o array de binarios actual:
+        byte[] activeData = myWhiteboard.getDataToSave();
+
+        //adiciona o snapshot à entidade:
+        snap.setUserApp(user);
+        snap.setImageDate(actualdate);
+        snap.setImage(activeData);
+        snapFacade.addSnapshot(snap);
+    }
+
+    public UserAppFacade getUserFacade() {
+        return userFacade;
+    }
+
+    public void setUserFacade(UserAppFacade userFacade) {
+        this.userFacade = userFacade;
     }
 
     public UserApp getUser() {
@@ -50,6 +97,30 @@ public class UserBean implements Serializable {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public SnapshotFacade getSnapFacade() {
+        return snapFacade;
+    }
+
+    public void setSnapFacade(SnapshotFacade snapFacade) {
+        this.snapFacade = snapFacade;
+    }
+
+    public MyWhiteboard getMyWhiteboard() {
+        return myWhiteboard;
+    }
+
+    public void setMyWhiteboard(MyWhiteboard myWhiteboard) {
+        this.myWhiteboard = myWhiteboard;
+    }
+
+    public Date getActualdate() {
+        return actualdate;
+    }
+
+    public void setActualdate(Date actualdate) {
+        this.actualdate = actualdate;
     }
 
 }
