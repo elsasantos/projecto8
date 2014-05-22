2023cfd1ff84265772da9d520cc2a1e1ca0e57b5
Guilherme Pereira <jorgeguilhermepereira@gmail.com>
2014-05-22 17:56:57 +0100

    Merge branch 'master' of https://github.com/elsasantos/projecto8
    Conflicts:
    	src/java/pt/uc/aor/edcodisrt/endpoint/MyWhiteboard.java
    	web/resources/css/cssLayout.css
    	web/resources/template/WhiteboardTemplate.xhtml

--- src/java/pt/uc/aor/edcodisrt/jsfbean/LogoutBean.java
+++ src/java/pt/uc/aor/edcodisrt/jsfbean/LogoutBean.java
@@ -13,40 +13,43 @@ import javax.servlet.http.HttpSession;
  * This JSF/CDI Managed Bean provides a way for users to log out of the
  * application.
  */
-@Named(value = "logoutBean")
+@Named
 @RequestScoped
 public class LogoutBean {
 
-    // Built in logging is used now, but will be replaced in future with
-    // 3rd party solution. Logging does what System.out.println() does and
-    // more!
+ 
     private static Logger log = Logger.getLogger(LogoutBean.class.getName());
 
-    public String logout() {
-        // Notice the redirect syntax. The forward slash means start at
-        // the root of the web application.
-        String destination = "/index?faces-redirect=true";
+    public LogoutBean() {
+    }
 
-        // FacesContext provides access to other container managed objects,
-        // such as the HttpServletRequest object, which is needed to perform
-        // the logout operation.
+    public static Logger getLog() {
+        return log;
+    }
+
+    public static void setLog(Logger log) {
+        LogoutBean.log = log;
+    }
+    
+
+    public String logout() {
+        System.out.println("entrou");
+        String destination = "/faces/index?faces-redirect=true";
+        
         FacesContext context = FacesContext.getCurrentInstance();
         HttpServletRequest request
                 = (HttpServletRequest) context.getExternalContext().getRequest();
 
         try {
-            // added May 12, 2014
             HttpSession session = request.getSession();
             session.invalidate();
 
-            // this does not invalidate the session but does null out the user Principle
             request.logout();
         } catch (ServletException e) {
             log.log(Level.SEVERE, "Failed to logout user!", e);
             destination = "/loginerror?faces-redirect=true";
-        }
-
-        return destination; // go to destination
+        }     
+        return destination;
     }
 
     public String loggedUser() {
