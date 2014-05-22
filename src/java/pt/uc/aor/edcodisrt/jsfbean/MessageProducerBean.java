e174451b974512fa93cf6aa9d040288db05e6fc2
Guilherme Pereira <jorgeguilhermepereira@gmail.com>
2014-05-22 11:58:23 +0100

    Merge origin/master
    Conflicts:
    	web/index.xhtml

---
+++ src/java/pt/uc/aor/edcodisrt/jsfbean/MessageProducerBean.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.uc.aor.edcodisrt.jsfbean;
+
+import java.nio.ByteBuffer;
+import javax.annotation.Resource;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.Topic;
+
+/**
+ *
+ * @author Guilherme Pereira
+ */
+@Named(value = "messageProducerBean")
+@RequestScoped
+public class MessageProducerBean {
+
+    @Resource(mappedName = "jms/myTopic")
+    private Topic myTopic;
+    @Inject
+    @JMSConnectionFactory("jms/myTopicFactory")
+    private JMSContext context;
+
+
+    public MessageProducerBean() {
+    }
+
+
+    public void sendMessage(ByteBuffer message) {
+        byte[] b = new byte[message.capacity()];
+        message.get(b, 0, b.length);
+        context.createProducer().send(myTopic, b);
+        System.out.println("sending message " + message.toString());
+    }
+
+}
+
+
